9d2c0c97fbe1c510a9efa8df658f995d7e1b3f4b
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-08-24 14:01:36 +0200

    activate email

--- controllers/users/verificationByRequest.js
+++ controllers/users/verificationByRequest.js
@@ -17,7 +17,7 @@ const verificationByRequest = async (req, res, next) => {
       subject: "Подтверждения email",
       html: `<a  href="http://localhost:4040/api/users/verify/${user.verifyToken}" target="_blank">Подтвердить email</a>`,
     };
-    // await sendEmail(mail);
+    await sendEmail(mail);
     res.status(200).json({ message: "Verification email sent" });
   } catch (error) {
     next(error);
